0ecf5aec08786702d3df8336f159812db93776d9
Prince Ojha <princekumar29062001@gmail.com>
2023-04-20 19:40:45 +0530

    remain part is onclick chart update is pending

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,21 @@
+/* Show and hide sidebar */
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("aside");
+menuBtn.addEventListener("click", () => {
+  sidebar.style.display = "block";
+});
 
+closeBtn.addEventListener("click", () => {
+  sidebar.style.display = "none";
+});
+// change theme
+const themeBtn = document.querySelector(".theme-btn");
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeBtn.querySelector("span:first-child").classList.toggle("active");
+  themeBtn.querySelector("span:last-child").classList.toggle("active");
+});
 // const chart= document.querySelector("#chart").getContext('2d');
 //create new chart instance
 // new Chart(chart,{
@@ -19,7 +36,7 @@
 //             borderwidth:2
 //         },
 //     ]
-// }, 
+// },
 // options:{
 //     responsive:true
 //   }
@@ -63,7 +80,7 @@
 //           }
 //         }
 //       }
-      
+
 //     });
 //   })
 //   .catch(error => {
@@ -105,54 +122,59 @@
 //   chart.update();
 // });
 
-
 // Define the chart configurations for each chart type
 const chartConfigs = {
   line: {
     type: "line",
     data: {
       labels: [],
-      datasets: [{
-        label: "BTC Price (usd)",
-        data: [],
-        borderColor: "red",
-        borderWidth: 2
-      }]
+      datasets: [
+        {
+          label: "BTC Price (usd)",
+          data: [],
+          borderColor: "red",
+          borderWidth: 2,
+        },
+      ],
     },
     options: {
-      responsive: true
-    }
+      responsive: true,
+    },
   },
   bar: {
     type: "bar",
     data: {
       labels: [],
-      datasets: [{
-        label: "BTC Price (usd)",
-        data: [],
-        backgroundColor: "blue",
-        borderWidth: 2
-      }]
+      datasets: [
+        {
+          label: "BTC Price (usd)",
+          data: [],
+          backgroundColor: "blue",
+          borderWidth: 2,
+        },
+      ],
     },
     options: {
-      responsive: true
-    }
+      responsive: true,
+    },
   },
-  pie: {
+  bar: {
     type: "pie",
     data: {
       labels: [],
-      datasets: [{
-        label: "BTC Price (usd)",
-        data: [],
-        backgroundColor: "Yellow",
-        borderWidth: 2
-      }]
+      datasets: [
+        {
+          label: "BTC Price (usd)",
+          data: [],
+          backgroundColor: "Yellow",
+          borderWidth: 2,
+        },
+      ],
     },
     options: {
-      responsive: true
-    }
-  }
+      responsive: true,
+    },
+  },
 };
 
 // Create a new Chart.js chart instance with the default line chart configuration
@@ -163,7 +185,7 @@ const chart = new Chart(ctx, chartConfigs.line);
 const dropdown = document.getElementById("chart-type");
 
 // Add an event listener to the dropdown to update the chart instance
-dropdown.addEventListener("change", event => {
+dropdown.addEventListener("change", (event) => {
   // Get the selected chart type
   const chartType = event.target.value;
 
@@ -175,38 +197,84 @@ dropdown.addEventListener("change", event => {
 });
 
 // Open a WebSocket connection to the CoinCap API
-const socketUrl = "wss://ws.coincap.io/prices?assets=bitcoin";
-const socket = new WebSocket(socketUrl);
+const loadChartData = ((name)=>{
+  console.log(name);
 
-socket.addEventListener("open", event => {
-  console.log("WebSocket connection opened");
-});
+  const socketUrl = `wss://ws.coincap.io/prices?assets=${name}`;
+  const socket = new WebSocket(socketUrl);
+  console.log(socket);
+  
+  socket.addEventListener("open", (event) => {
+    console.log("WebSocket connection opened");
+  });
+  
+  // Handle incoming WebSocket messages and update the chart data
+  socket.addEventListener("message", (event) => {
+    const data = JSON.parse(event.data);
+    console.log(data);
+    console.log(event);
+    chart.data.labels.push(new Date().toLocaleTimeString());
+    chart.data.datasets[0].data.push(data[`${name}`]);
 
-// Handle incoming WebSocket messages and update the chart data
-socket.addEventListener("message", event => {
-  const data = JSON.parse(event.data);
-  chart.data.labels.push(new Date().toLocaleTimeString());
-  chart.data.datasets[0].data.push(data.bitcoin);
-  chart.update();
-});
-
-
-/* Show and hide sidebar */
-const menuBtn = document.querySelector('#menu-btn');
-const closeBtn = document.querySelector('#close-btn');
-const sidebar = document.querySelector('aside');
-menuBtn.addEventListener('click', ()=>
-{
-  sidebar.style.display='block';
+    chart.update();
+  });
 })
 
-closeBtn.addEventListener('click',()=>{
-  sidebar.style.display='none';
+
+
+
+const investmentContainer = document.querySelector(".investment-container");
+
+
+
+const formatedDate = ((id)=>{
+  const date = new Date(id);
+
+  const formattedDate = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'long',
+    timeStyle: 'short',
+  }).format(date);
+
+  return formattedDate;
 })
- // change theme
- const themeBtn = document.querySelector('.theme-btn');
-  themeBtn.addEventListener('click', ()=>{
-    document.body.classList.toggle('dark-theme');
-    themeBtn.querySelector('span:first-child').classList.toggle('active');
-    themeBtn.querySelector('span:last-child').classList.toggle('active');
-  })
+
+async function fetchData() {
+  try {
+    const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en`);
+    const data = await response.json();
+    console.log(data.length);
+    console.log(data);
+    data.map((item) => {
+      const investmentElement = document.createElement('div');
+      investmentElement.className = 'investment col-14';
+      investmentElement.innerHTML = `
+        <img src="${item.image}">
+        <h4>${item.name}</h4>
+        <div class="date-time">
+           
+            <small class="text-muted">${ formatedDate( item.ath_date)}</small>
+        </div>
+        <div class="bonds">
+          <p>${item.market_cap_rank}</p>
+          <small class="text-muted">Rank</small>
+        </div>                    
+        <div class="amount">
+          <h4>${item.current_price}</h4>
+          <small class="danger">${item.ath_change_percentage}</small>
+        </div>
+      `;
+      investmentContainer.appendChild(investmentElement);
+
+      // Add event listener to investment container
+      investmentElement.addEventListener('click', () => {
+        loadChartData(item.id)
+        console.log(`You clicked on ${item.name}!`);
+        // Add code to do something when container is clicked
+      });
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchData();
